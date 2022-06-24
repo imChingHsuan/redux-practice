187cfb287ef5b2d19b6bb3754fbc67d703ba7bb3
ChingHsuanLi <lshe543211@gmail.com>
2022-06-24 19:48:23 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const count = useSelector((data) => data.counter);
+  const isLogged = useSelector((data) => data.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>hello shelly</h1>
+      <h2>count: {count}</h2>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement(2));
+        }}
+      >
+        -
+      </button>
+      <h2>Am I signin? {isLogged ? <p>YES</p> : <p>NOT YET</p>}</h2>
+    </div>
+  );
+}
+
+export default App;
